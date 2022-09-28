cdb97dcdf37edf64e0dcce0ca57c64475c652b04
857986269 <114558870+857986269@users.noreply.github.com>
2022-09-28 14:27:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,12 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: '',
+  APP_ID: 'wx9a4d0790222f8372',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '',
+  APP_SECRET: '6b54023c848beb647ebc40d4ddf3787f',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -55,25 +55,25 @@ const USER_CONFIG = {
 
   // 每日一言的内容类型
   // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-  LITERARY_PREFERENCE: '',
+  LITERARY_PREFERENCE: '漫画',
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [
+  USERS: [ogJ-46BHgcSqHywLOU3iXtnG4x1E
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '亲爱的小徐同学，早上好吖',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'wxid_aoojoyfOr9qm22',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '7ZGGbrrJDpAGT6Jz3GhdwQlQDKnaKtDoWGsJthgy7Sw',
       // 所在省份
-      province: '广东',
+      province: '河北',
       // 所在城市
-      city: '肇庆',
+      city: '邢台',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '03-26',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -82,14 +82,14 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '老婆', year: '1996', date: '09-09',
+          type: '*生日', name: '小徐同学', year: '2003', date: '02-24',
         },
         {
-          type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '在一起纪念日', year: '2021', date: '12-23',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '小马同学', year: '2002', date: '01-15',
         },
         {
           type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
